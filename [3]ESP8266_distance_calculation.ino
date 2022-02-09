36e848747a7f9bc632375f4e94fb31bc20ae50a4
GabrielSiega <82450712+GabrielSiega@users.noreply.github.com>
2022-02-09 14:54:22 +0800

    Update [3]ESP8266_distance_calculation.ino

--- [3]ESP8266_distance_calculation.ino
+++ [3]ESP8266_distance_calculation.ino
@@ -1,50 +1,57 @@
-#define TRIGGER 5
-#define ECHO    4
- 
+#define BLYNK_PRINT Serial
+
+
 #include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include <BlynkSimpleEsp8266.h>
+#define TRIGGERPIN D1
+#define ECHOPIN    D2
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "your auth code";
 
-// Set these to run example.
-//#define FIREBASE_HOST "speedy-nodemcu.firebaseio.com"
-//#define FIREBASE_AUTH "A1Az2TkGnSyTjHxK7EPTEYZRNi4nPE3L1yCtnamA"
-#define WIFI_SSID "Aviavatar"
-#define WIFI_PASSWORD "qwertyui"
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ssid name";//your wifi name
+char pass[] = "ssid pass"; //your wifi password
 
-void setup() {
+WidgetLCD lcd(V1);
+
+void setup()
+{
+  // Debug console
   Serial.begin(9600);
+pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
 
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting"); 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-  
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  pinMode(TRIGGER, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(BUILTIN_LED, OUTPUT);
+  lcd.clear(); //Use it to clear the LCD Widget
+  lcd.print(0, 0, "Distance in cm"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+  // Please use timed events when LCD printintg in void loop to avoid sending too many commands
+  // It will cause a FLOOD Error, and connection will be dropped
 }
- 
-void loop() {
- 
+
+void loop()
+{
+  lcd.clear();
+  lcd.print(0, 0, "Distance in cm"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
   long duration, distance;
-  digitalWrite(TRIGGER, LOW);  
-  delayMicroseconds(2); 
+  digitalWrite(TRIGGERPIN, LOW);  
+  delayMicroseconds(3); 
   
-  digitalWrite(TRIGGER, HIGH);
-  delayMicroseconds(10); 
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(12); 
   
-  digitalWrite(TRIGGER, LOW);
-  duration = pulseIn(ECHO, HIGH);
+  digitalWrite(TRIGGERPIN, LOW);
+  duration = pulseIn(ECHOPIN, HIGH);
   distance = (duration/2) / 29.1;
-  
   Serial.print(distance);
-  Serial.println("Centimeter:");
-  Firebase.setInt("Distance",distance);
+  Serial.println("Cm");
+  lcd.print(7, 1, distance);
+  Blynk.run();
+
   delay(1000);
+
 }
